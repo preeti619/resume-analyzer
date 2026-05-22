bf0a17139ee9ea817f1884a72ddf80a04deb231c
Preeti <sainipreeti239@gmail.com>
2026-05-22 13:28:18 +0530

    Update summary.py

--- helper/summary.py
+++ helper/summary.py
@@ -4,7 +4,7 @@ from groq import Groq
 def generate_summary(resume_text):
     try:
         client = Groq(
-            api_key="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" )
+            api_key="enter yours" )
 
         prompt = f"""
         Read this resume and create:
